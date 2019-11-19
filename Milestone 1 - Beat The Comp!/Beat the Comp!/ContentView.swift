67c5b9f4a348fdca600e0583aa584e5c58d00cf2
krishmurjani <42816556+krishmurjani@users.noreply.github.com>
2019-11-20 00:47:18 +0530

    Add files via upload

--- Milestone 1 - Beat The Comp!/Beat the Comp!/ContentView.swift
+++ Milestone 1 - Beat The Comp!/Beat the Comp!/ContentView.swift
@@ -11,24 +11,24 @@ import SwiftUI
 struct ContentView: View {
     @State private var choices = ["Rock", "Paper", "Scissors"]
     @State private var emojiChoices = ["✊", "🖐", "✌️"]
-    @State private var currentChoice =  0
     @State private var decider = ["WIN", "LOSE"]
     @State private var score = 0
-    @State private var randomDecider = Int.random(in: 0...1)
     @State private var randomChoice = Int.random(in: 0...2)
     @State private var questions = 0
     @State private var check = false
+    @State private var checkReset = false
+    @State private var needTo = Bool.random()
     
     var body: some View {
         VStack(spacing: 5) {
             
             HStack {
                 Spacer()
-                Text("Score: \(self.score)")
+                Text("Score: \(self.score)/10")
                     .fontWeight(.medium)
             }.padding(.trailing)
             
-            Text("You have to \(decider[randomDecider])!")
+            Text("You have to \(needTo ? "WIN" : "LOSE")!")
                 .font(.system(size: 45))
                 .fontWeight(.semibold)
             
@@ -37,7 +37,7 @@ struct ContentView: View {
             
             VStack {
                 Text("\(emojiChoices[randomChoice])")
-                    .font(.custom("Times New Roman", size: 150))
+                    .font(.custom("Times New Roman", size: 120))
                 Text("\(choices[randomChoice])")
                     .font(.system(size: 35))
                     .fontWeight(.bold)
@@ -47,17 +47,14 @@ struct ContentView: View {
             VStack {
                 
                 HStack(spacing: 60) {
-                    ForEach(0..<3) { currentChoice in
+                    ForEach(0..<3) { move in
                         Button(action: {
-                            self.buttonTapped()
+                            self.buttonTapped("\(self.choices[move])")
                         }) { VStack {
-                            Text("\(self.emojiChoices[currentChoice])")
+                            Text("\(self.emojiChoices[move])")
                                 .font(.custom("Times New Roman", size: 80))
-                            Text("\(self.choices[currentChoice])")
-                                .font(.system(size: 30))
+                            Text("\(self.choices[move])")                         .font(.system(size: 25))
                                 .fontWeight(.medium)
-                                .foregroundColor(Color.blue)
-                            
                             }
                         }
                     }
@@ -66,92 +63,71 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
             }
+            
+            Button(action: {
+                self.checkResetButton()
+            }) {
+                Text("Reset")
+                    .foregroundColor(.red)
+                    .font(.system(size: 20))
+            }
         }
         .alert(isPresented: $check) {
-            Alert(title: Text("Game Over"), message: Text("Tap continue to restart"), dismissButton: .default(Text("Continue")) {
+            Alert(title: Text("Game Over"), message: Text("Your score is \(score). Tap OK to restart!"), dismissButton: .default(Text("OK")) {
                 self.endGame()
                 })
         }
+        .alert(isPresented: $checkReset) {
+            Alert(title: Text("Are you sure?"), message: Text("If you tap yes, you will lose your current progress. Do you wish to continue?"), primaryButton: .destructive(Text("Yes")) {
+                self.resetTapped()
+            }, secondaryButton: .cancel((Text("No"))))
+        }
     }
     
-    func buttonTapped () {
+    func buttonTapped (_ userMove: String) {
+        let currentMove = choices[randomChoice]
         questions += 1
-        if randomChoice == 0 {
-            if randomDecider == 0 {
-                if currentChoice == randomChoice+1 {
-                    score += 1
-                    self.reset()
-                }
-                else if currentChoice == randomChoice+2 {
-                    score -= 1
-                    self.reset()
-                }
+        
+        if currentMove == userMove {
+            score -= 1
+            reset()
+        } else if needTo {
+            if currentMove == "Rock" && userMove == "Paper" {
+                score += 1
+                reset()
+            } else if currentMove == "Paper" && userMove == "Scissors" {
+                score += 1
+                reset()
+            } else if currentMove == "Scissors" && userMove == "Rock" {
+                score += 1
+                reset()
+            } else {
+                score -= 1
+                reset()
             }
-            else {
-                if currentChoice == randomChoice+1 {
-                    score -= 1
-                    self.reset()
-                }
-                else if currentChoice == randomChoice+2 {
-                    score += 1
-                    self.reset()
-                }
-            }
-        }
-        else if randomChoice == 1 {
-            if randomDecider == 0 {
-                if currentChoice == randomChoice+1 {
-                    score += 1
-                    self.reset()
-                }
-                else if currentChoice == randomChoice-1 {
-                    score -= 1
-                    self.reset()
-                }
-            }
-            else {
-                if currentChoice == randomChoice+1 {
-                    score -= 1
-                    self.reset()
-                }
-                else if currentChoice == randomChoice-1 {
-                    score += 1
-                    self.reset()
-                }
+        } else {
+            if currentMove == "Rock" && userMove == "Paper" {
+                score -= 1
+                reset()
+            } else if currentMove == "Paper" && userMove == "Scissors" {
+                score -= 1
+                reset()
+            } else if currentMove == "Scissors" && userMove == "Rock" {
+                score -= 1
+                reset()
+            } else {
+                score += 1
+                reset()
             }
         }
-        else if randomChoice == 2 {
-            if randomDecider == 0 {
-                if currentChoice == randomChoice-2 {
-                    score += 1
-                    self.reset()
-                }
-                else if currentChoice == randomChoice-1 {
-                    score -= 1
-                    self.reset()
-                }
-            }
-            else {
-                if currentChoice == randomChoice-2 {
-                    score -= 1
-                    self.reset()
-                }
-                else if currentChoice == randomChoice-1 {
-                    score += 1
-                    self.reset()
-                }
-            }
-        }
-        self.reset()
         
         if questions == 10 {
             check = true
         }
-        
     }
     
     func reset() {
-        randomDecider = Int.random(in: 0...1)
+        needTo = Bool.random()
         randomChoice = Int.random(in: 0...2)
     }
     
@@ -160,6 +136,17 @@ struct ContentView: View {
         score = 0
         reset()
     }
+    
+    func checkResetButton() {
+        checkReset = true
+    }
+    
+    func resetTapped() {
+        score = 0
+        needTo = Bool.random()
+        randomChoice = Int.random(in: 0...2)
+    }
+
 }
 
 
